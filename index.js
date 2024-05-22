6648a3a3049a43830802411fc4e22987d094ce6b
Alexig2 <alex4342on@gmail.com>
2024-05-22 13:27:21 -0400

    Metade da segunda aula

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ require("dotenv").config();
 const conn = require("./db/conn");
 const Usuario = require("./models/Usuario");
 
+const express = require("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+app.get("/usuarios/novo", (req, res) => {
+    res.sendFile(`${__dirname}/views/formUsuario.html`);
+});
+
 conn
     .sync()
     .then(() => {
